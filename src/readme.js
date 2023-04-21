12ebeafbf06c9f287deda4b47519b37eabf0d0c3
ObcbO <49872497+ObcbO@users.noreply.github.com>
2023-04-21 12:41:39 +0800

    Update readme.js

--- src/readme.js
+++ src/readme.js
@@ -2,66 +2,47 @@ const fs = require('fs');
 const path = require('path');
 const sharp = require('sharp');
 module.exports = {
-    allToOne
+    combinePngs
 }
 
-async function allToOne(inputDir, outputFilePath) {
-  try {
-    // Read all PNG images
-    const images = fs
-      .readdirSync(inputDir)
-      .filter(file => path.extname(file) === '.png')
-      .map(file => path.join(inputDir, file))
-      .filter(image => {
-        try {
-          sharp(image).metadata();
-          return true;
-        } catch (err) {
-          console.log(`Error reading file ${image}: ${err.message}`);
-          return false;
-        }
-      });
-
-    // Calculate the aspect ratio of each image
-    const imageRatios = await Promise.all(images.map(async image => {
-      const metadata = await sharp(image).metadata();
-      return metadata.width / metadata.height;
-    }));
-
-    // Calculate the width and height of the final image
-    const imageCount = images.length;
-    const totalRatio = imageRatios.reduce((sum, ratio) => sum + ratio, 0);
-    const targetRatio = 16 / 9;
-    const targetHeight = 1080;
-    const targetWidth = targetRatio * targetHeight;
-    const actualRatio = targetWidth / (targetHeight * imageCount / totalRatio);
-    const width = actualRatio <= targetRatio ? targetWidth : targetHeight * imageCount / totalRatio;
-    const height = actualRatio <= targetRatio ? width / targetRatio : targetHeight;
-
-    // Create the final image
-    const outputImage = sharp({
-      create: {
-        width: Math.round(width),
-        height: Math.round(height),
-        channels: 4,
-        background: { r: 255, g: 255, b: 255, alpha: 0 }
-      }
+/**
+ * Combine all PNG images in the specified directory into a single PNG image with a ratio of 16:9
+ * @param {string} inputDir The directory where the original PNG images are stored
+ * @param {string} outputPath The directory and filename where the combined PNG image will be saved
+ * @returns {Promise<string>} The file path of the combined PNG image
+ */
+async function combinePngs(inputDir, outputPath) {
+  const ratio = 16 / 9; // The target ratio
+  // Read all files in the directory
+  const files = await fs.promises.readdir(inputDir);
+  
+  // Filter out PNG files
+  const pngFiles = files.filter(file => path.extname(file) === '.png');
+  
+  // Calculate the width and height of the combined image
+  const width = Math.floor(pngFiles.length / ratio);
+  const height = Math.floor(width / ratio);
+  
+  // Create a Sharp instance
+  const sharpInstance = sharp({
+    create: {
+      width: width,
+      height: height,
+      channels: 4,
+      background: { r: 0, g: 0, b: 0, alpha: 0 }
+    }
+  });
+  
+  // Loop through the PNG files and composite them into the combined image
+  pngFiles.forEach((file, index) => {
+    sharpInstance.overlayWith(`${inputDir}/${file}`, {
+      gravity: 'northwest',
+      top: Math.floor(index / ratio) * Math.floor(height / ratio),
+      left: (index % ratio) * Math.floor(width / ratio)
     });
-
-    // Resize and composite all images into the final image
-    await Promise.all(images.map(async (imagePath, index) => {
-      const buffer = await sharp(imagePath).resize(Math.round(width / imageCount), Math.round(height)).toBuffer();
-      const x = Math.round(index * width / imageCount);
-      const y = 0;
-      return outputImage.composite([{ input: buffer, left: x, top: y }]);
-    }));
-
-    // Save the final image
-    await outputImage.toFile(outputFilePath);
-
-    // Return the output file path
-    return outputFilePath;
-  } catch (err) {
-    throw err;
-  }
+  });
+  
+  // Save the combined image and return the file path
+  const { filename } = await sharpInstance.toFile(outputPath);
+  return filename;
 }
